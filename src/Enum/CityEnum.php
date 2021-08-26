bdda7fb1637398fa397564586cf1043b04ddbf3c
Karmazin <vyacheslavkarmazin@gmail.com>
2021-08-26 15:38:46 +0200

    initial

---
+++ src/Enum/CityEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AvailabilityBundle\Enum;
+
+class CityEnum
+{
+    public const MOSCOW        = 'MSK';
+    public const YEKATERINBURG = 'EKB';
+    public const VLADIVOSTOK   = 'VVO';
+    public const NUR_SULTAN    = 'AST';
+}
---
+++ src/Enum/VisaTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AvailabilityBundle\Enum;
+
+class VisaTypeEnum
+{
+    public const B1 = 'B1';
+    public const F1 = 'F1';
+    public const L2 = 'L2';
+}
